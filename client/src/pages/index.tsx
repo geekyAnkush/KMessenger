cd786b8fb8f7ff8275db53a6cda2d66d85636390
Kumar Ankush <000.kumarankush@gmail.com>
2023-01-27 16:14:05 +0530

    add: NEXTAUTH Authentication

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <div>hello</div>;
+  const { data, status } = useSession();
+  console.log("here is the data -> ", data);
+  console.log("here is the status -> ", status);
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign In</button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
