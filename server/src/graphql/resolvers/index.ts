be3d14bfab058d53f87d7e3d07bdf9fb3b1ec5f9
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-01 15:18:48 +0530

    add: create conversation typedefs

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,5 @@
 import userResolvers from "./user";
+import conversationResolvers from "./conversation";
 import merge from "lodash.merge";
-const resolvers = merge({}, userResolvers);
+const resolvers = merge({}, userResolvers, conversationResolvers);
 export default resolvers;
