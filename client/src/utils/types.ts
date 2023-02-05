692ebe8233a1e52dc8f2ee5e738f9e61c1d9206b
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 17:38:21 +0530

    add: utility funtions and types

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,33 +1,94 @@
-// users
+import {
+  ConversationPopulated,
+  MessagePopulated,
+} from "../../../backend/src/util/types";
+
+/**
+ * Users
+ */
+export interface CreateUsernameVariables {
+  username: string;
+}
+
 export interface CreateUsernameData {
   createUsername: {
     success: boolean;
     error: string;
   };
 }
-export interface CreateUsernameVariables {
+
+export interface SearchUsersInputs {
   username: string;
 }
 
-export interface SearchUsersInput {
-  username: string;
+export interface SearchUsersData {
+  searchUsers: Array<SearchedUser>;
 }
 
 export interface SearchedUser {
   id: string;
   username: string;
 }
-export interface SearchUsersData {
-  searchUsers: Array<SearchedUser>;
+
+/**
+ * Messages
+ */
+export interface MessagesData {
+  messages: Array<MessagePopulated>;
 }
 
-// conversations
+export interface MessagesVariables {
+  conversationId: string;
+}
+
+export interface SendMessageVariables {
+  id: string;
+  conversationId: string;
+  senderId: string;
+  body: string;
+}
+
+export interface MessagesSubscriptionData {
+  subscriptionData: {
+    data: {
+      messageSent: MessagePopulated;
+    };
+  };
+}
+
+/**
+ * Conversations
+ */
 export interface CreateConversationData {
   createConversation: {
     conversationId: string;
   };
 }
 
-export interface CreateConversationInput {
-  participantIds: Array<string>;
+export interface ConversationsData {
+  conversations: Array<ConversationPopulated>;
+}
+
+export interface ConversationCreatedSubscriptionData {
+  subscriptionData: {
+    data: {
+      conversationCreated: ConversationPopulated;
+    };
+  };
+}
+
+export interface ConversationUpdatedData {
+  conversationUpdated: {
+    conversation: Omit<ConversationPopulated, "latestMessage"> & {
+      latestMessage: MessagePopulated;
+    };
+    addedUserIds: Array<string> | null;
+    removedUserIds: Array<string> | null;
+  };
+}
+
+export interface ConversationDeletedData {
+  conversationDeleted: {
+    id: string;
+  };
 }
