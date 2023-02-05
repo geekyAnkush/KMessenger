21fc27a072313eeff0a2a66f498f4c350e82fd10
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 17:34:48 +0530

    add: graphql operations

--- client/src/graphql/operations/conversation.ts
+++ client/src/graphql/operations/conversation.ts
@@ -1,7 +1,36 @@
 import { gql } from "@apollo/client";
 
+const ConversationFields = `
+  conversations {
+    id
+    participants {
+      user {
+        id
+        username
+      }
+      seenStatus
+    }
+    latestMessage {
+      id
+      sender {
+        id
+        username
+      }
+      body
+      createdAt
+    }
+    updatedAt
+}
+`;
+
 const ConversationOperations = {
-  Queries: {},
+  Queries: {
+    conversations: gql`
+      query Conversations {
+        ${ConversationFields}
+      }
+    `,
+  },
   Mutations: {
     createConversation: gql`
       mutation createConversation($participantIds: [String]!) {
