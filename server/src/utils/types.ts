d8bdd8db0836a933f83354898edf906e086b6f71
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 17:41:54 +0530

    add: utility functions

--- server/src/utils/types.ts
+++ server/src/utils/types.ts
@@ -1,25 +1,89 @@
-import { PrismaClient } from "@prisma/client";
-import { ISODateString } from "next-auth";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { PubSub } from "graphql-subscriptions";
+import { Context } from "graphql-ws/lib/server";
+import {
+  conversationPopulated,
+  participantPopulated,
+} from "../graphql/resolvers/conversations";
+import { messagePopulated } from "../graphql/resolvers/messages";
 
-// users
-interface User {
+/**
+ * Server Configuration
+ */
+export interface Session {
+  user?: User;
+}
+
+export interface GraphQLContext {
+  session: Session | null;
+  prisma: PrismaClient;
+  pubsub: PubSub;
+}
+
+export interface SubscriptionContext extends Context {
+  connectionParams: {
+    session?: Session;
+  };
+}
+
+/**
+ * Users
+ */
+export interface User {
   id: string;
   username: string;
-  image: string;
-  email: string;
-  emailVerified: boolean;
-  name: string;
 }
-export interface Session {
-  user: User;
-  expires: ISODateString;
-}
+
 export interface CreateUsernameResponse {
   success?: boolean;
   error?: string;
 }
-export interface GraphQLContext {
-  session: Session | null;
-  prisma: PrismaClient;
-  // pubsub
+
+export interface SearchUsersResponse {
+  users: Array<User>;
+}
+
+/**
+ * Messages
+ */
+export interface SendMessageArguments {
+  id: string;
+  conversationId: string;
+  senderId: string;
+  body: string;
+}
+
+export interface SendMessageSubscriptionPayload {
+  messageSent: MessagePopulated;
+}
+
+export type MessagePopulated = Prisma.MessageGetPayload<{
+  include: typeof messagePopulated;
+}>;
+
+/**
+ * Conversations
+ */
+export type ConversationPopulated = Prisma.ConversationGetPayload<{
+  include: typeof conversationPopulated;
+}>;
+
+export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
+  include: typeof participantPopulated;
+}>;
+
+export interface ConversationCreatedSubscriptionPayload {
+  conversationCreated: ConversationPopulated;
+}
+
+export interface ConversationUpdatedSubscriptionData {
+  conversationUpdated: {
+    conversation: ConversationPopulated;
+    addedUserIds: Array<string>;
+    removedUserIds: Array<string>;
+  };
+}
+
+export interface ConversationDeletedSubscriptionPayload {
+  conversationDeleted: ConversationPopulated;
 }
