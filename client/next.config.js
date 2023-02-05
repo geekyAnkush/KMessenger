70be49d8735a546bfe50f7706aaf1bac8d627e86
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 18:02:44 +0530

    add: backed api routes

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GRAPHQL_API_URL: "http://localhost:4000/graphql",
+    GRAPHQL_SUBSCRIPTION_URL: "ws://localhost:4000/graphql/subscriptions",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
