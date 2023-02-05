c599fbd140cb74b997ce50b18ad7220c15181d84
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 17:31:40 +0530

    completed auth component

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -2,82 +2,94 @@ import { useMutation } from "@apollo/client";
 import { Button, Center, Image, Input, Stack, Text } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { signIn } from "next-auth/react";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-import UserOperations from "../../graphql/operations/user";
-import { CreateUsernameData, CreateUsernameVariables } from "../../utils/types";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import UserOperations from "../../graphql/operations/users";
+import { CreateUsernameData, CreateUsernameVariables } from "../../util/types";
 
-export interface IAuthProps {
+interface AuthProps {
   session: Session | null;
   reloadSession: () => void;
 }
 
-const Auth: React.FunctionComponent<IAuthProps> = ({
-  session,
-  reloadSession,
-}) => {
+const Auth: React.FC<AuthProps> = ({ session, reloadSession }) => {
   const [username, setUsername] = useState("");
-  const [createUsername, { loading, error }] = useMutation<
+
+  const [createUsername, { data, loading, error }] = useMutation<
     CreateUsernameData,
     CreateUsernameVariables
   >(UserOperations.Mutations.createUsername);
+
   const onSubmit = async () => {
     if (!username) return;
+
     try {
-      const { data } = await createUsername({ variables: { username } });
+      const { data } = await createUsername({
+        variables: {
+          username,
+        },
+      });
+
       if (!data?.createUsername) {
         throw new Error();
       }
-      if (data?.createUsername.error) {
+
+      if (data.createUsername.error) {
         const {
           createUsername: { error },
         } = data;
-        throw new Error(error);
+
+        toast.error(error);
+        return;
       }
-      toast.success("username successfully created! 🚀 ");
+
+      toast.success("Username successfully created");
+
+      /**
+       * Reload session to obtain new username
+       */
       reloadSession();
-    } catch (error: any) {
-      toast.error(error?.message);
-      console.log("onSubmit error -> ", error);
+    } catch (error) {
+      toast.error("There was an error");
+      console.log("onSubmit error", error);
     }
   };
+
   return (
     <Center height="100vh">
-      <Stack align="center">
+      <Stack spacing={8} align="center">
         {session ? (
           <>
             <Text fontSize="3xl">Create a Username</Text>
             <Input
               placeholder="Enter a username"
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setUsername(event.target.value)
+              }
             />
-            <Button
-              colorScheme={"blackAlpha"}
-              width="100%"
-              onClick={onSubmit}
-              isLoading={loading}
-            >
+            <Button onClick={onSubmit} width="100%" isLoading={loading}>
               Save
             </Button>
           </>
         ) : (
-          <Stack align="center" spacing={8}>
-            <Text fontSize="3xl">Kmessages</Text>
+          <>
+            <Image height={100} src="/images/imessage-logo.png" />
+            <Text fontSize="4xl">KMesenger</Text>
+            <Text width="70%" align="center">
+              Sign in with Google to send unlimited free messages to your
+              friends
+            </Text>
             <Button
               onClick={() => signIn("google")}
-              leftIcon={
-                <Image height="20px" src="/images/googlelogo.png" alt="logo" />
-              }
-              colorScheme={"blackAlpha"}
+              leftIcon={<Image height="20px" src="/images/googlelogo.png" />}
             >
               Continue with Google
             </Button>
-          </Stack>
+          </>
         )}
       </Stack>
     </Center>
   );
 };
-
 export default Auth;
