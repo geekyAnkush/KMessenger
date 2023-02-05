b1185d0bac0ae1226900907f490bc573efc282ae
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-05 17:31:23 +0530

    theme configuration

--- client/src/chakra/theme.ts
+++ client/src/chakra/theme.ts
@@ -1,24 +1,24 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const custom = {
-  colors: {
-    brand: {
-      100: "#3d84f7",
-    },
-  },
-  styles: {
-    global: {
-      body: {
-        bg: "#2D313C",
-        color: "#F7FAFC",
+export const theme = extendTheme(
+  { config },
+  {
+    colors: {
+      brand: {
+        100: "#3D84F7",
       },
     },
-    ...config,
-  },
-};
-export const theme = extendTheme(custom);
+    styles: {
+      global: () => ({
+        body: {
+          bg: "whiteAlpha.200",
+        },
+      }),
+    },
+  }
+);
