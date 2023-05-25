79d51d3be8a23713998b2bfe5d7acdabb707a26a
Kumar Ankush <54018647+geekyAnkush@users.noreply.github.com>
2023-05-25 16:51:47 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GRAPHQL_API_URL: "http://localhost:4000/graphql",
-    GRAPHQL_SUBSCRIPTION_URL: "ws://localhost:4000/graphql/subscriptions",
-  },
+//   env: {
+//     GRAPHQL_API_URL: "http://localhost:4000/graphql",
+//     GRAPHQL_SUBSCRIPTION_URL: "ws://localhost:4000/graphql/subscriptions",
+//   },
   typescript: {
     ignoreBuildErrors: true,
   },
