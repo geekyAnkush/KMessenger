0e3a4517d7a34834cf7326c4370a5eabf436fbaa
Kumar Ankush <54018647+geekyAnkush@users.noreply.github.com>
2023-05-25 16:56:28 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-//   env: {
-//     GRAPHQL_API_URL: "http://localhost:4000/graphql",
-//     GRAPHQL_SUBSCRIPTION_URL: "ws://localhost:4000/graphql/subscriptions",
-//   },
+  env: {
+    GRAPHQL_API_URL: "https://kmessenger-backend.onrender.com/graphql",
+    GRAPHQL_SUBSCRIPTION_URL: "ws://kmessenger-backend.onrender.com/graphql/subscriptions",
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
